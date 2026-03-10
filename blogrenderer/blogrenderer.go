a114c7525a656f4d51eda835f8e730ea3280e482
pylbrecht <pylbrecht@mailbox.org>
2026-03-10 08:18:09 +0100

    feat: templating

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,67 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+	return &PostRenderer{templ}, nil
+}
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+type PostView struct {
+	Title, SanitisedTitle, Description, Body string
+	Tags                                     []string
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.ReplaceAll(p.Title, " ", "-"))
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	r, err := NewPostRenderer()
+
+	if err != nil {
+		return err
+	}
+
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	r, err := NewPostRenderer()
+
+	if err != nil {
+		return err
+	}
+
+	if err := r.templ.ExecuteTemplate(w, "index.gohtml", posts); err != nil {
+		return err
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module helloworld/hello
 
-go 1.23.0
+go 1.24.0
+
+require github.com/approvals/go-approval-tests v0.0.0-20211008131110-0c40b30e0000
