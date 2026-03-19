262e76f4887302023e075c60ba00f87e70548fa9
pylbrecht <pylbrecht@mailbox.org>
2026-03-19 20:56:20 +0100

    feat: generics
    I just read over this particular chapter, as it wasn't that much code.

---
+++ generics/generic_test.go
@@ -0,0 +1,29 @@
+package generics
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "world")
+	})
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %d", got)
+	}
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("didn't want %d", got)
+	}
+}
